b7f610bc411ab556397c49f456c76811570a6f7d
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-07-18 01:04:32 +0530

    Update JobDetails.jsx

--- job-portal-client/src/Pages/JobDetails.jsx
+++ job-portal-client/src/Pages/JobDetails.jsx
@@ -7,7 +7,7 @@ const JobDetails = () => {
   const { id } = useParams();
   const [job, setJob] = useState([]);
   useEffect(() => {
-    fetch(`http://localhost:3000/all-jobs/${id}`)
+    fetch(`https://get-hired-eta.vercel.app/all-jobs/${id}`)
       .then((res) => res.json())
       .then((data) => setJob(data));
   }, []);
