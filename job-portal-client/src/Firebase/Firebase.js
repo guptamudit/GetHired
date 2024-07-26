0f6a3480437c9da3844c49565d6963f2b2316cbe
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-07-26 12:34:42 +0530

    Update Firebase.js

--- job-portal-client/src/Firebase/Firebase.js
+++ job-portal-client/src/Firebase/Firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXaVo6y4N-PG-MSO0luDWxZZ_yhSvi1PM",
+  apiKey: "API KEY",
   authDomain: "mern-job-portal-a7798.firebaseapp.com",
   projectId: "mern-job-portal-a7798",
   storageBucket: "mern-job-portal-a7798.appspot.com",
-  messagingSenderId: "880062420662",
-  appId: "1:880062420662:web:2ba2d56663886aac6cb817",
-  measurementId: "G-7M97H8JS3M",
+  messagingSenderId: "messagingSenderId",
+  appId: "APP ID",
+  measurementId: "measurementId",
 };
 
 // Initialize Firebase
